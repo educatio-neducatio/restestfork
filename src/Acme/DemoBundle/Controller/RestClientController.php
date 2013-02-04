08bfb84d17c520231232628561b6453e15133dc9
Michal Strzelczyk <mstrzelczyk@n-educatio.pl>
2013-02-04 13:06:28 +0100

    Dodanie akcji wysylajacej plik i restowej pobierajacej requesta z plikiem

---
+++ src/Acme/DemoBundle/Controller/RestClientController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/restclient")
+ */
+class RestClientController extends Controller
+{
+  /**
+   * @Route("/user/{id}")
+   * 
+   * @param type $id 
+   */
+  public function getUserAction($id)
+  {
+    $service_url = "http://192.168.103.55/mstrzelczyk/resttest/web/users/{$id}";
+       $curl = curl_init($service_url);
+       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($curl, CURLOPT_POST, false);
+       //curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+       $curl_response = curl_exec($curl);
+       curl_close($curl);
+    return new Response($curl_response);
+  }
+  /**
+   * @Route("/createuser/{username}/{password}")
+   * 
+   * @param type $id 
+   */
+  public function CreateUserAction($username, $password)
+  {
+    $service_url = "http://192.168.103.55/mstrzelczyk/resttest/web/users";
+    $curl = curl_init($service_url);
+    $curl_post_data = array(
+        "username" => $username,
+        "password" => $password,
+    );
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+    $curl_response = curl_exec($curl);
+    curl_close($curl);
+    return new Response($curl_response);
+  }
+  
+  
+  /**
+   * @Route("/sendfile")
+   * 
+   * @param type $id 
+   */
+  public function SendFileAction()
+  {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
+    curl_setopt($ch, CURLOPT_URL, "http://192.168.103.55/mstrzelczyk/resttest/web/files");
+    curl_setopt($ch, CURLOPT_POST, true);
+    // same as <input type="file" name="file_box">
+    //print_r($_SERVER);
+    //die();
+    $post = array(
+        "Resource"=>"@/home/mstrzelczyk/NMS_INT_2.zip",
+    );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post); 
+    return new Response(curl_exec($ch));
+  }
+}
---
+++ src/Acme/DemoBundle/Controller/RestController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+
+class RestController extends FOSRestController
+{
+    public function getUsersAction()
+    {
+      $view = View::create()
+        ->setStatusCode(200)
+        ->setData('users!');
+      return $this->get('fos_rest.view_handler')->handle($view);
+    } // "get_users"     [GET] /users
+
+    public function newUsersAction()
+    {} // "new_users"     [GET] /users/new
+
+    public function postUsersAction(Request $request)
+    {
+      if ('POST' === $request->getMethod()) {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $view = View::create()
+          ->setStatusCode(200)
+          ->setData("username: {$username}   password: {$password}");
+        return $this->get('fos_rest.view_handler')->handle($view);
+      } else {
+        return new Response ('nie mam posta');
+      }
+    } // "post_users"    [POST] /users
+
+    public function getUserAction($slug)
+    {
+      $view = View::create()
+        ->setStatusCode(200)
+        ->setData("user {$slug}");
+      return $this->get('fos_rest.view_handler')->handle($view);
+    } // "get_user"      [GET] /users/{slug}
+    
+    
+    public function postFilesAction(Request $request)
+    {
+      if ('POST' === $request->getMethod()) {;
+        $view = View::create()
+          ->setStatusCode(200)
+          ->setData($_FILES);
+        return $this->get('fos_rest.view_handler')->handle($view);
+      } else {
+        return new Response ('nie mam posta');
+      }
+    } // "post_files"    [POST] /files
+}
